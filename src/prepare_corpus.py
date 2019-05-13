b6ea7c8c673eddee9d93694034357ca0d254a93a
zolizoli <zoltan.varju@gmail.com>
2019-05-13 20:24:50 +0200

    add todos

--- src/prepare_corpus.py
+++ src/prepare_corpus.py
@@ -4,13 +4,17 @@ import json
 with open('data/raw/Presidential_speeches.json', 'r') as f:
     raw_corpus = json.load(f)
 
+# president_party = {'Barack Obama': 'democrat'}
 periods = [(1789, 1849), (1850, 1865), (1866, 1918), (1919, 1945),
            (1946, 1964), (1965, 1980), (1980, 1991), (1992, 2008),
            (2009, 2017)]
 corpora = {}
 for e in raw_corpus:
+    # President X.Y. YEAR
     year = int(re.findall(r'\d{4}', e['title'])[0])
+    # president = ....
     period = [p for p in periods if p[0] <= year <= p[1]][0]
+    # party = president_party[president]
     cidx = periods.index(period)
     speech1 = e['speech1']
     speech2 = e['speech2']

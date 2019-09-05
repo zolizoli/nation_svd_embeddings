7e00df6c0e3bac84c6232e77cdcccf0a1c44cb85
zolizoli <zoltan.varju@gmail.com>
2019-09-05 10:31:23 +0200

    detect changes, measure their correlation

--- src/procrustes.py
+++ src/procrustes.py
@@ -2,6 +2,7 @@ from os import listdir
 from os.path import isdir, join
 
 import numpy as np
+from scipy.stats import pearsonr, spearmanr
 from scipy.spatial import procrustes
 from scipy.spatial.distance import cosine
 from sklearn.neighbors import NearestNeighbors
@@ -44,16 +45,17 @@ wd_shifts = {}
 for wd in sorted_vocab:
     if wd not in wd_shifts.keys():
         wd_shifts[wd] = []
-    initial = NearestNeighbors(n_neighbors=6, algorithm='ball_tree').fit(loaded_models[0].U)
+    initial = NearestNeighbors(n_neighbors=100, algorithm='ball_tree').fit(loaded_models[0].U)
     initial_neighbors = initial.kneighbors(loaded_models[0].U[loaded_models[0].vocabulary.index(wd)].reshape(1, -1))
-    final = NearestNeighbors(n_neighbors=6, algorithm='ball_tree').fit(loaded_models[-1].U)
+    final = NearestNeighbors(n_neighbors=100, algorithm='ball_tree').fit(loaded_models[-1].U)
     final_neighbors = final.kneighbors(loaded_models[-1].U[loaded_models[-1].vocabulary.index(wd)].reshape(1, -1))
     initial_neighbors = initial_neighbors[1].tolist()[0][1:]
     final_neighbors = final_neighbors[1].tolist()[0][1:]
     initial_words = [loaded_models[0].vocabulary[j] for j in initial_neighbors
-                     if loaded_models[0].vocabulary[j] in common_vocab]
+                     if loaded_models[0].vocabulary[j] in common_vocab][:5]
     final_words = [loaded_models[-1].vocabulary[j] for j in final_neighbors
-                   if loaded_models[-1].vocabulary[j] in common_vocab]
+                   if loaded_models[-1].vocabulary[j] in common_vocab][:5]
+    print(len(final_words), len(initial_words))
     for model in loaded_models:
         period = loaded_models.index(model)
         wdidx = model.vocabulary.index(wd)
@@ -68,3 +70,16 @@ for wd in sorted_vocab:
             sim = cosine_similarity(model.U[wdidx], model.U[iw])
             t = ('final', period, e, sim)
             wd_shifts[wd].append(t)
+
+# strong > 0.4
+# 0.4 > moderate > 0.2
+# weak < 0.2
+for wd, results in wd_shifts.items():
+    initial_results = [e[3] for e in results if e[0] == 'initial']
+    final_results = [e[3] for e in results if e[0] == 'final']
+    pearson = pearsonr(initial_results, final_results)
+    spearman = spearmanr(initial_results, final_results)
+    if pearson[0] > 0.2 or pearson[0] < -0.2:
+        print('p', wd, pearson[0])
+    if spearman.correlation > 0.6 or spearman.correlation < -0.6:
+        print('s', wd, spearman.correlation)
